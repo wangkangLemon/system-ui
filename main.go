43548c9208f828c5f6ccdf20d6d5bfdf48d819e8
coldstar <safeie@163.com>
2017-03-16 20:30:51 +0800

    提交Go WEB基础

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"git.vodjk.com/golang/common/modules/setting"
+	"github.com/baa-middleware/accesslog"
+	"github.com/baa-middleware/gzip"
+	"github.com/baa-middleware/recovery"
+	"github.com/baa-middleware/static"
+	"gopkg.in/baa.v1"
+)
+
+func main() {
+	app := baa.Default()
+	app.Use(recovery.Recovery())
+
+	// gzip
+	if !setting.Debug {
+		app.Use(gzip.Gzip(gzip.Options{
+			CompressionLevel: 9,
+		}))
+	}
+
+	// accesslog
+	if setting.Config.MustBool("http.access_open", false) {
+		app.Use(accesslog.Logger())
+	}
+
+	// static file
+	app.Use(static.Static("/", "./public/", false, nil))
+
+	// run
+	addr := setting.Config.MustString("http.address", "0.0.0.0")
+	port := setting.Config.MustString("http.port", "8001")
+	app.Run(addr + ":" + port)
+}
