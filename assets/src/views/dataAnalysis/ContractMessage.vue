135453f10ee8f041476aadaa43a9cc6c335416cd
huangrui <huangrui@vodjk.com>
2017-03-27 15:15:48 +0800

    数据解析

--- assets/src/views/dataAnalysis/ContractMessage.vue
+++ assets/src/views/dataAnalysis/ContractMessage.vue
@@ -1,13 +1,241 @@
 <!--企业签约信息-->
-<style lang='scss' rel='stylesheet/scss' scoped>
+<style lang='scss' scoped rel="stylesheet/scss">
     @import "../../utils/mixins/mixins";
     @import "../../utils/mixins/table";
+
+    .table-container {
+        border: 1px solid #ededed;
+        .add {
+            padding: 10px 20px;
+            .el-icon-upload2 {
+                padding-right: 5px;
+            }
+        }
+        .import-container {
+            padding: 20px;
+        }
+        .main-container {
+            background: #fff;
+            padding: 20px;
+            .block {
+                text-align: right;
+                margin-top: 10px;
+            }
+        }
+        .showDetail {
+            .avatar {
+                border: 1px solid #ededed;
+                display: inline-block;
+                vertical-align: top;
+            }
+            .info {
+                display: inline-block;
+                vertical-align: top;
+                > p {
+                    > span {
+                        display: inline-block;
+                        width: px2rem(100);
+                        text-align: right;
+                        padding-right: px2rem(10);
+                    }
+                }
+            }
+        }
+    }
 </style>
 <template>
-    <article>
-        签约
+    <article class="table-container">
+        <!--导入弹窗-->
+        <el-dialog v-model="isImport" title="导入企业签约信息" size="tiny">
+            导入
+        </el-dialog>
+        <!--详情-->
+        <el-dialog class="showDetail" title="测试添加企业签约信息" v-model="showDetial">
+            <div class="info">
+                <p><span>门店数量：</span>80</p>
+                <p><span>签约店员数量：</span> <i class="iconfont icon-oslash"></i>1200</p>
+                <p><span>对方联系人：</span><i class="el-icon-message"></i> 12@vodjk.com</p>
+                <p><span>联系方式：</span> 正常</p>
+                <p><span>签约人：</span> 女</p>
+                <p><span>签约时间：</span> 北京市朝阳区</p>
+                <p><span>录入系统时间：</span>2017-03-17 12:333</p>
+            </div>
+        </el-dialog>
+        <section class="add">
+            <el-button @click="isImport = true"><i class="el-icon-upload2"></i>导入</el-button>
+        </section>
+        <div class="main-container">
+            <section class="search">
+                <div>
+                    <label>连锁名称</label>
+                    <el-input class="name" v-model="searchName"></el-input>
+                </div>
+                <div>
+                    <label>是否录入门店</label>
+                    <el-select v-model="inputDepSelect">
+                        <el-option
+                                v-for="(item, index) in inputDeps"
+                                :label="item.name"
+                                :value="item.id"
+                                :key="item.id">
+                        </el-option>
+                    </el-select>
+                </div>
+                <div>
+                    <label>是否录入店员</label>
+                    <el-select v-model="inputMemSelect">
+                        <el-option
+                                v-for="(item, index) in inputMembers"
+                                :label="item.name"
+                                :value="item.id"
+                                :key="item.id">
+                        </el-option>
+                    </el-select>
+                </div>
+                <div>
+                    <label>签约时间</label>
+                    <div class="time-container">
+                        <el-date-picker
+                                v-model="createTime"
+                                type="date"
+                                placeholder="开始时间">
+                        </el-date-picker>
+                        <el-date-picker
+                                v-model="endTime"
+                                type="date"
+                                placeholder="结束时间">
+                        </el-date-picker>
+                    </div>
+                </div>
+                <div>
+                    <label>签约人</label>
+                    <el-select v-model="constructorSelect">
+                        <el-option
+                                v-for="(item, index) in constructor"
+                                :label="item.name"
+                                :value="item.id"
+                                :key="item.id">
+                        </el-option>
+                    </el-select>
+                </div>
+            </section>
+            <el-table border :data="tableData">
+                <el-table-column
+                        prop="name"
+                        label="连锁名称"
+                        width="100%">
+                </el-table-column>
+                <el-table-column
+                        prop="contractDep"
+                        label="签约门店"
+                        width="200">
+                </el-table-column>
+                <el-table-column
+                        prop="inputDep"
+                        label="录入门店"
+                        width="200">
+                </el-table-column>
+                <el-table-column
+                        prop="contractMem"
+                        label="签约店员"
+                        width="200">
+                </el-table-column>
+                <el-table-column
+                        prop="inputMember"
+                        label="录入店员"
+                        width="200">
+                </el-table-column>
+                <el-table-column prop="operate" label="查看">
+                    <template scope="scope">
+                        <el-button type="text" size="small" @click="showDetial = true">
+                            连锁信息
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <section class="block">
+                <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="currentPage4"
+                        :page-sizes="[1, 2]"
+                        layout="total, sizes, ->, prev, pager, next, jumper"
+                        :total="4">
+                </el-pagination>
+            </section>
+        </div>
     </article>
 </template>
 <script lang="babel">
-    export default {}
+    export default {
+        data () {
+            return {
+                searchName: '',        // 连锁名称
+                inputDepSelect: '',
+                inputDeps: [            // 是否录入门店
+                    {
+                        id: 0,
+                        name: '已录入',
+                    },
+                    {
+                        id: 1,
+                        name: '未录入',
+                    }
+                ],
+                inputMemSelect: '',
+                inputMembers: [            // 是否录入店员
+                    {
+                        id: 0,
+                        name: '已录入',
+                    },
+                    {
+                        id: 1,
+                        name: '未录入',
+                    }
+                ],
+                constructorSelect: '',
+                constructor: [
+                    {
+                        id: 1,
+                        name: '测试营销员'
+                    },
+                    {
+                        id: 2,
+                        name: '管理员'
+                    },
+                ],
+                createTime: '',
+                endTime: '',
+                isImport: false,    // 导入弹窗显示状态
+                showDetial: false,     // 是否显示详情对话框
+                currentPage4: 1, // 分页当前显示的页数
+                tableData: [
+                    {
+                        id: 1,
+                        name: '销售',
+                        contractDep: '13920307216',
+                        inputDep: '13@vod.com',
+                        contractMem: '2133',
+                        inputMember: '102,2202',
+                        status: '正常'
+                    }
+                ]
+            }
+        },
+        methods: {
+            forbidden () {
+                this.isForbidden = false
+            },
+            handleSizeChange (val) {
+                console.log(`每页 ${val} 条`)
+                // 当切换每页条数得时候 获取当前第一页得数据
+                this.handleCurrentChange(1)
+            },
+            handleCurrentChange (val) {
+                this.currentPage = val
+                console.log(`当前页: ${val}`)
+                // 以下获取当页数据
+            }
+        }
+    }
 </script>
--- assets/src/views/dataAnalysis/Export.vue
+++ assets/src/views/dataAnalysis/Export.vue
@@ -2,12 +2,115 @@
 <style lang='scss' rel='stylesheet/scss' scoped>
     @import "../../utils/mixins/mixins";
     @import "../../utils/mixins/table";
+
+    .main-container {
+        padding: 20px;
+    }
 </style>
 <template>
-    <article>
-        导出
+    <article class="main-container">
+        <section class="search">
+            <div>
+                <label>状态</label>
+                <el-select v-model="statusSelect">
+                    <el-option
+                            v-for="(item, index) in exportStatus"
+                            :label="item.name"
+                            :value="item.id"
+                            :key="item.id">
+                    </el-option>
+                </el-select>
+            </div>
+            <div>
+                <label>时间</label>
+                <div class="time-container">
+                    <el-date-picker
+                            v-model="createTime"
+                            type="date"
+                            placeholder="开始时间">
+                    </el-date-picker>
+                    <el-date-picker
+                            v-model="endTime"
+                            type="date"
+                            placeholder="结束时间">
+                    </el-date-picker>
+                </div>
+            </div>
+        </section>
+        <el-table
+                :data="data" border>
+            <el-table-column
+                    prop="name"
+                    label="名称">
+            </el-table-column>
+            <el-table-column
+                    prop="status"
+                    label="状态"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="createTime"
+                    label="创建时间"
+                    width="180">
+            </el-table-column>
+            <el-table-column prop="operate" label="操作" width="180">
+                <template scope="scope">
+                    <el-button type="text" size="small">
+                        下载
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <section class="block">
+            <el-pagination
+                    :current-page="currentPage"
+                    :page-sizes="[100, 200, 300, 400]"
+                    layout="total, sizes, ->, prev, pager, next, jumper"
+                    :total="400">
+            </el-pagination>
+        </section>
     </article>
 </template>
 <script lang="babel">
-    export default {}
+    export default {
+        data () {
+            return {
+                createTime: '',
+                endTime: '',
+                currentPage: 1,
+                statusSelect: 0,
+                ip: '',
+                data: [
+                    {
+                        id: 1,
+                        name: '上海市普陀区金沙江路 1518 弄',
+                        status: '10.1.1.1',
+                        createTime: '21-22-2'
+                    }
+                ],
+                exportStatus: [
+                    {
+                        id: 0,
+                        name: '未选择'
+                    },
+                    {
+                        id: 1,
+                        name: '已完成'
+                    },
+                    {
+                        id: 2,
+                        name: '待处理'
+                    },
+                    {
+                        id: 3,
+                        name: '进行中'
+                    },
+                    {
+                        id: 4,
+                        name: '失败'
+                    }
+                ]
+            }
+        }
+    }
 </script>
