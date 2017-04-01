f8c0094ea959b786a55ffe66b6022bea0d84031f
huangrui <huangrui@vodjk.com>
2017-04-01 19:23:15 +0800

    文章管理静态页

--- assets/src/views/article/Category.vue
+++ assets/src/views/article/Category.vue
@@ -1,58 +1,94 @@
-<!--分类管理-->
-<style lang='scss' scoped rel="stylesheet/scss">
+<!--文章分类管理-->
+<style lang='scss' rel="stylesheet/scss">
     @import "../../utils/mixins/mixins";
-    @import "../../utils/mixins/table";
 
-    .classify-container {
-        margin-top: 20px;
-        @extend %justify;
-        .left-content {
-            display: inline-block;
-            vertical-align: top;
-            width: 35%;
-
-            .el-icon-plus {
-                margin-right: 10px;
-            }
-            .el-tree {
-                width: auto;
-                margin-top: 10px;
-            }
+    .article-category {
+        .add-category {
+            text-align: right;
         }
+        .article-category-content {
+            background: #fff;
+            margin-top: 20px;
+            @extend %justify;
+            padding: 20px;
+            .left-content {
+                border-right: 1px solid #ededed;
+                display: inline-block;
+                vertical-align: top;
+                width: 35%;
+                .el-icon-plus {
+                    margin-right: 10px;
+                }
+                .el-tree {
+                    width: auto;
+                    border: none;
+                }
+            }
 
-        .right-content {
-            width: 62%;
-            display: inline-block;
-            vertical-align: top;
-            padding-bottom: 20px;
+            .right-content {
+                width: 62%;
+                display: inline-block;
+                vertical-align: top;
+                padding-bottom: 20px;
 
+            }
         }
     }
 
 </style>
 <template>
-    <article class="classify-container">
-        <section class="left-content">
-            <el-button type="primary"><i class="el-icon-plus"></i>添加分类</el-button>
-
-            <el-tree class="leftSubTree" :highlight-current="true" :data="leftSubTreeData"
-                     :props="defaultProps" @node-click="leftClassifyClick"></el-tree>
-        </section>
-        <section class="right-content">
-            <el-tabs type="border-card">
-                <el-tab-pane label="修改分类">用户管理</el-tab-pane>
-                <el-tab-pane label="添加子分类">配置管理</el-tab-pane>
-                <el-tab-pane label="移动分类">角色管理</el-tab-pane>
-                <el-tab-pane label="移动内容">定时任务补偿</el-tab-pane>
-            </el-tabs>
-        </section>
+    <article class="article-category">
+        <div class="add-category">
+            <el-button type="primary" @click="addNewCategory = true"><i class="el-icon-plus"></i>添加分类</el-button>
+        </div>
+        <article class="article-category-content">
+            <section class="left-content" v-if="!addNewCategory">
+                <el-tree
+                        :default-expanded-keys="[2]"
+                        :default-checked-keys="[2]"
+                        node-key="id"
+                        :show-checkbox="true"
+                        :check-strictly="true"
+                        class="leftSubTree"
+                        :highlight-current="true"
+                        :data="categoryData"
+                        :props="defaultProps"
+                        @node-click="leftClassifyClick"></el-tree>
+            </section>
+            <section class="right-content">
+                <el-tabs v-model="activeName" v-if="!addNewCategory" @tab-click="handleClick">
+                    <el-tab-pane label="修改分类" name="first"></el-tab-pane>
+                    <el-tab-pane label="添加子分类" name="second"></el-tab-pane>
+                    <el-tab-pane label="移动分类" name="third"></el-tab-pane>
+                    <el-tab-pane label="移动分类下内容" name="fourth"></el-tab-pane>
+                    <el-tab-pane label="删除分类" name="fifth"></el-tab-pane>
+                </el-tabs>
+                <div>
+                    <el-form label-position="top" class="addForm" :model="form" :rules="rules" ref="form">
+                        <el-form-item prop="name" label="名称">
+                            <el-input v-model="form.name" auto-complete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item prop="sort" label="排序">
+                            <el-input v-model="form.sort" auto-complete="off"></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button type="primary" @click="submit('form')">修改</el-button>
+                        <el-button v-if="addNewCategory" @click="addNewCategory = false">取消</el-button>
+                    </div>
+                </div>
+            </section>
+        </article>
     </article>
 </template>
 <script lang="babel">
+    let id = 1000
     export default {
         data () {
             return {
-                leftSubTreeData: [
+                addNewCategory: false,
+                activeName: 'second',
+                categoryData: [
                     {
                         id: 1,
                         label: '一级 1',
@@ -77,6 +113,19 @@
                     children: 'children',
                     label: 'label'
                 },
+                form: {
+                    name: '',
+                    sort: ''
+                },
+                rules: {
+                    name: [
+                        {
+                            required: true,
+                            message: '必填项',
+                            trigger: 'blur'
+                        }
+                    ]
+                }
             }
         },
         created () {
@@ -87,6 +136,37 @@
                 console.log('调用接口数据 并赋值给blockData')
                 this.currentClass = item.label
             },
+            handleClick (tab, event) {
+                console.log(tab, event)
+            },
+            submit (form) { // 表单提交
+                this.$refs[form].validate((valid) => {
+                    if (valid) {
+                        console.log(1)
+                    } else {
+                        return false
+                    }
+                })
+            },
+            append (store, data) {
+                store.append({id: id++, label: 'testtest', children: []}, data)
+            },
+            remove (store, data) {
+                store.remove(data)
+            },
+            renderContent (h, {node, data, store}) {
+//                return (
+//                    <span>
+//                        <span>
+//                          <span>{node.label}</span>
+//                        </span>
+//                        <span style="float: right; margin-right: 20px">
+//                          <el-button size="mini" on-click={ () => this.append(store, data) }>Append</el-button>
+//                          <el-button size="mini" on-click={ () => this.remove(store, data) }>Delete</el-button>
+//                        </span>
+//                    </span>
+//                )
+            }
         }
     }
 </script>
---
+++ assets/src/views/article/Content.vue
@@ -0,0 +1,287 @@
+<!--管理员-->
+<style lang='scss' rel="stylesheet/scss">
+    @import "../../utils/mixins/mixins";
+    @import "../../utils/mixins/topSearch";
+
+    .table-container {
+        border: 1px solid #ededed;
+        .add {
+            background: #ededed;
+            padding: px2rem(10) px2rem(20);
+            border-bottom: 1px solid #ededed;
+        }
+        .main-container {
+            padding: 20px;
+            background: #fff;
+            .search {
+                @extend %top-search-container;
+            }
+            .block {
+                text-align: right;
+                margin-top: 10px;
+            }
+        }
+        .showDetail {
+            .avatar {
+                border: 1px solid #ededed;
+                display: inline-block;
+                vertical-align: top;
+            }
+            .info {
+                display: inline-block;
+                vertical-align: top;
+                > p {
+                    > span {
+                        display: inline-block;
+                        width: px2rem(100);
+                        text-align: right;
+                        padding-right: px2rem(10);
+                    }
+                }
+            }
+        }
+        .forbidden-content {
+            text-align: center;
+            i {
+                &:before {
+                    display: block;
+                    font-size: px2rem(60);
+                    color: #f8bb86;
+                    padding-bottom: px2rem(40);
+                }
+            }
+            h1 {
+                font-size: px2rem(20);
+            }
+            p {
+                margin: px2rem(10);
+                span {
+                    color: red;
+                }
+            }
+        }
+    }
+</style>
+<template>
+    <article class="table-container">
+        <!--删除弹窗-->
+        <delete-dialog :text="itemName" :isShow="deletDialog" :callback="deleteItem"></delete-dialog>
+        <!--详情-->
+        <el-dialog class="showDetail" title="查看管理员账号" v-model="showDetial">
+            <div class="avatar">
+                <img src="http://sys.yst.vodjk.dev/assets/img/user-default-female.jpg?00da903dc4d95b13b46f"/>
+            </div>
+            <div class="info">
+                <p><span>测试营销员</span></p>
+                <p><span>Mobile：</span> <i class="iconfont icon-oslash"></i>13012332324</p>
+                <p><span>Email：</span><i class="el-icon-message"></i> 12@vodjk.com</p>
+                <p><span>状态：</span> 正常</p>
+                <p><span>性别：</span> 女</p>
+                <p><span>地址：</span> 北京市朝阳区</p>
+                <p><span>注册时间：</span>2017-03-17 12:333</p>
+            </div>
+        </el-dialog>
+        <!--添加/编辑表单-->
+        <el-dialog v-model="addForm">
+            <el-form :model="form" :rules="rules" ref="form">
+                <el-form-item prop="role" label="角色" :label-width="formLabelWidth">
+                    <el-select v-model="form.role" placeholder="角色">
+                        <el-option label="管理员" value="管理员"></el-option>
+                        <el-option label="编辑" value="编辑"></el-option>
+                        <el-option label="营销" value="营销"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item prop="name" label="姓名" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" placeholder="管理员姓名" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item prop="sex" label="性别" :label-width="formLabelWidth">
+                    <el-radio class="radio" v-model="form.sex" :label="0">男</el-radio>
+                    <el-radio class="radio" v-model="form.sex" :label="1">女</el-radio>
+                </el-form-item>
+                <el-form-item prop="mobile" label="手机号" :label-width="formLabelWidth">
+                    <el-input v-model="form.mobile" type="number" placeholder="手机号" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item prop="email" label="邮箱" :label-width="formLabelWidth">
+                    <el-input v-model="form.email" placeholder="邮箱" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item prop="pass" label="密码" :label-width="formLabelWidth">
+                    <el-input type="password" v-model="form.pass" placeholder="密码" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="地址" :label-width="formLabelWidth">
+                    <el-input v-model="form.address" placeholder="地址" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item prop="status" label="状态" :label-width="formLabelWidth">
+                    <el-radio class="radio" v-model="form.status" :label="0">正常</el-radio>
+                    <el-radio class="radio" v-model="form.status" :label="1">禁用</el-radio>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="addForm = false">取 消</el-button>
+                <el-button type="primary" @click="submit('form')">确 定</el-button>
+            </div>
+        </el-dialog>
+        <section class="add">
+            <!--点击添加 form数据取邮箱/手机号 密码-->
+            <el-button icon="plus" @click="addForm = true">添加</el-button>
+        </section>
+        <div class="main-container">
+            <section class="search">
+                <section>
+                    <i>姓名</i>
+                    <el-input class="name" v-model="name" placeholder="请输入姓名"></el-input>
+                </section>
+            </section>
+            <el-card class="box-card">
+                <el-table border :data="tableData">
+                    <el-table-column
+                            prop="title"
+                            label="标题">
+                    </el-table-column>
+                    <el-table-column
+                            prop="category_name"
+                            label="类别"
+                            width="200">
+                    </el-table-column>
+                    <el-table-column
+                            prop="status_name"
+                            label="状态"
+                            width="200">
+                    </el-table-column>
+                    <el-table-column
+                            prop="create_time_name"
+                            label="创建时间"
+                            width="200">
+                    </el-table-column>
+                    <el-table-column prop="operate" label="操作">
+                        <template scope="scope">
+                            <el-button type="text" size="small" @click="showDetial = true">
+                                详情
+                            </el-button>
+                            <el-button type="text" size="small" @click="addForm = true">
+                                修改
+                                <!--点击详情 form数据变成当前管理员的信息-->
+                            </el-button>
+                            <el-button type="text" size="small" @click="handleDelete(scope.$index, scope.row)">
+                                删除
+                            </el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                <section class="block">
+                    <el-pagination
+                            @size-change="handleSizeChange"
+                            @current-change="handleCurrentChange"
+                            :current-page="currentPage4"
+                            :page-sizes="[1, 2]"
+                            layout="total, sizes, ->, prev, pager, next, jumper"
+                            :total="4">
+                    </el-pagination>
+                </section>
+            </el-card>
+        </div>
+    </article>
+</template>
+<script lang="babel">
+    import deleteDialog from '../component/dialog/Delete'
+    export default {
+        components: {
+            deleteDialog
+        },
+        data () {
+            let validateName = (rule, value, callback) => {
+                if ((value || '') === '') {
+                    callback(new Error('请输入姓名'))
+                }
+                callback()
+            }
+            let validateEmail = (rule, value, callback) => {
+                if (!(this.form.mobile || '').match(/^1[34578]\d{9}$/) && !(value || '').match(/^\w+([-+.]\w+)*@\w+([-+.]\w+)*.\w+([-+.]\w+)*$/)) {
+                    callback(new Error('邮箱或手机号至少填写一个'))
+                }
+                callback()
+            }
+            let validatePass = (rule, value, callback) => {
+                if ((value || '') === '') {
+                    callback(new Error('请输入密码'))
+                }
+                callback()
+            }
+            return {
+                itemName: '',           // 要删除项名称
+                deletDialog: false,     // 删除弹窗
+                showDetial: false,     // 是否显示详情对话框
+                form: {                // 表单属性值
+                    name: '',          // 姓名
+                    role: '编辑',       // 权限
+                    mobile: '',        // 手机
+                    email: '',         // 邮箱
+                    pass: '',          // 密码
+                    address: '',       // 地址
+                    sex: 0,            // 性别
+                    status: 0          // 状态
+                },
+                rules: {
+                    name: [
+                        {validator: validateName, trigger: 'blur'}
+                    ],
+                    email: [
+                        {validator: validateEmail, trigger: 'blur'}
+                    ],
+                    pass: [
+                        {validator: validatePass, trigger: 'blur'}
+                    ]
+                },
+                formLabelWidth: '120px', // 表单label的宽度
+                addForm: false, // 表单弹窗是否显示
+                currentPage4: 1, // 分页当前显示的页数
+                name: '', // 搜索的姓名
+                tableData: [
+                    {
+                        id: 1,
+                        title: '销售',
+                        category_name: '13920307216',
+                        create_time_name: '2133',
+                        status_name: '正常'
+                    }
+                ]
+            }
+        },
+        created () {
+            xmview.setContentLoading(false)
+        },
+        methods: {
+            handleDelete (index, row) {
+                this.deletDialog = true
+                this.itemName = row.name
+            },
+            deleteItem (confirm) {
+                this.deletDialog = false
+                if (!confirm) {
+                    return false
+                }
+                // 以下执行接口删除动作
+                console.log(11)
+            },
+            submit (form) {
+                this.$refs[form].validate((valid) => {
+                    console.log(valid)
+                    if (valid) {
+                        console.log(1)
+                    } else {
+                        return false
+                    }
+                })
+            },
+            handleSizeChange (val) {
+                console.log(`每页 ${val} 条`)
+                // 当切换每页条数得时候 获取当前第一页得数据
+                this.handleCurrentChange(1)
+            },
+            handleCurrentChange (val) {
+                this.currentPage = val
+                console.log(`当前页: ${val}`)
+                // 以下获取当页数据
+            }
+        }
+    }
+</script>
