54962b1b9b139480bd42e491019a5b522147449f
jingziming <zimingcafe@gmail.com>
2017-04-19 22:02:47 +0800

    update Dockerfile dev

--- Dockerfile/dev/build.sh
+++ Dockerfile/dev/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-docker build -t hub.docker.vodjk.com:5000/yst/system-ui:dev .
-docker push hub.docker.vodjk.com:5000/yst/system-ui:dev
+docker build -t hub.docker.vodjk.com/yst/system-ui:dev .
+docker push hub.docker.vodjk.com/yst/system-ui:dev
