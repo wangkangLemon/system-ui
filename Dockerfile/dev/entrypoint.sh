c258d6ebfd8c9d44a30c3ba2bfb6626a63cd79bf
jingziming <zimingcafe@gmail.com>
2017-04-24 16:54:29 +0800

    临时解决前端接口配置问题(待调优)

--- Dockerfile/dev/entrypoint.sh
+++ Dockerfile/dev/entrypoint.sh
@@ -8,7 +8,7 @@ if [[ $BRANCH == "" ]]; then export BRANCH=develop; fi
 cd $GOPATH/src/git.vodjk.com/yst/system-ui
 git pull origin $BRANCH
 cd $GOPATH/src/git.vodjk.com/yst/system-ui/assets
-yarn run build
+yarn run build -- -h http://10.1.2.140
 cd $GOPATH/src/git.vodjk.com/yst/system-ui
 go build
 ./system-ui
