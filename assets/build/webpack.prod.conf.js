f065ad2690a2aecef13240c4e51724bd170563d9
huanghuixin <274919800@qq.com>
2017-04-27 19:19:58 +0800

    修改构建代码 让app.js每次的hash不同

--- assets/build/webpack.prod.conf.js
+++ assets/build/webpack.prod.conf.js
@@ -31,7 +31,8 @@ var webpackConfig = merge(baseWebpackConfig, {
         // http://vuejs.github.io/vue-loader/en/workflow/production.html
         new webpack.DefinePlugin({
             'process.env': env,
-            'process.apiHost': config.build.apiHost
+            'process.apiHost': config.build.apiHost,
+            'process.buildTime': '"' + new Date().toLocaleString() + '"'
         }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
