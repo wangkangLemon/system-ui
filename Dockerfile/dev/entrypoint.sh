1901ae73802d1bb1c20668a2274cf5514f395f10
coldstar <safeie@163.com>
2017-04-27 11:44:37 +0800

    更新配置适配docker dev环境

--- Dockerfile/dev/entrypoint.sh
+++ Dockerfile/dev/entrypoint.sh
@@ -8,7 +8,6 @@ if [[ $BRANCH == "" ]]; then export BRANCH=develop; fi
 cd $GOPATH/src/git.vodjk.com/yst/system-ui
 git pull origin $BRANCH
 cd $GOPATH/src/git.vodjk.com/yst/system-ui/assets
-cat config/config.js.default > config/config.js
 yarn run build
 cd $GOPATH/src/git.vodjk.com/yst/system-ui
 go build
