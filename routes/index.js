c9ee8c8f1768c18a444bfd7e8aedad2a981a992f
coldstar <safeie@163.com>
2017-05-02 16:36:34 +0800

    完善node结构

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,8 +9,8 @@ router.get('/', function(req, res, next) {
 
 // 所有的视图页面
 router.get('/view/*', function(req, res, next){
-  res.set('Content-Type', 'text/plain');
-  res.sendFile('../public/index.html');
+  res.set('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 /* ping for heartbeat */
