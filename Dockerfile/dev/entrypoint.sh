002da6ab5f0ac8bcd993762646d33c14d50950c2
coldstar <safeie@163.com>
2017-05-02 17:55:57 +0800

    更新纯node dockerfile

--- Dockerfile/dev/entrypoint.sh
+++ Dockerfile/dev/entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 export BRANCH=$1
 if [[ $BRANCH == "" ]]; then export BRANCH=develop; fi
 
-cd $GOPATH/src/git.vodjk.com/yst/system-ui
+cd /node/src/git.vodjk.com/yst/system-ui
 git pull origin $BRANCH
-cd $GOPATH/src/git.vodjk.com/yst/system-ui/assets
+cd /node/src/git.vodjk.com/yst/system-ui/assets
 yarn run build
-cd $GOPATH/src/git.vodjk.com/yst/system-ui
+cd /node/src/git.vodjk.com/yst/system-ui
 npm run start
