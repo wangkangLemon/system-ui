01a1aec2c7220046d18b99b1f319f330209420d7
chenchao <chenchao@vodjk.com>
2017-05-03 14:54:15 +0800

    调整 课程任务模板分类 添加分类的时候 页面样式

--- assets/src/views/company/courseTask/Coursetasktemplatecategory.vue
+++ assets/src/views/company/courseTask/Coursetasktemplatecategory.vue
@@ -51,7 +51,7 @@
         </section>
 
         <section class="right-container">
-            <div>
+            <div v-if="fetchParam.parent_id != 0">
                 <el-button :class="{'btn-selected': activeTab == 'edit'}" @click="activeTab = 'edit'">修改分类</el-button>
                 <el-button :class="{'btn-selected': activeTab == 'add'}" @click="activeTab = 'add'">添加子分类</el-button>
 
@@ -60,20 +60,23 @@
                 <el-button type="danger" @click="deleteCategory">删除分类</el-button>
             </div>
 
+            <div v-if="fetchParam.parent_id === 0">
+                <el-button type="primary">添加根节点</el-button>
+            </div>
             <el-card class="edit-content">
                 <el-form label-position="right" label-width="90px" :rules="rules" :model="fetchParam" ref="form">
                     <el-form-item label="分类名称" prop="name">
-                        <el-input v-model="fetchParam.name"></el-input>
+                        <el-input v-model="fetchParam.name" :disabled="fetchParam.parent_id == null"></el-input>
                     </el-form-item>
                     <el-form-item label="图片" prop="image">
                         <UploadImg ref="uploadImg" :defaultImg="fetchParam.image" :url="uploadImgUrl"
                                    :onSuccess="handleImgUploaded"></UploadImg>
                     </el-form-item>
                     <el-form-item label="分类排序" prop="sort">
-                        <el-input placeholder="最小的排在前面" v-model.number="fetchParam.sort"></el-input>
+                        <el-input :disabled="fetchParam.parent_id == null" placeholder="最小的排在前面" v-model.number="fetchParam.sort"></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="info" @click="submitForm">保存</el-button>
+                        <el-button :disabled="fetchParam.parent_id == null" type="info" @click="submitForm">保存</el-button>
                     </el-form-item>
                 </el-form>
             </el-card>
@@ -185,6 +188,7 @@
             // 左边的节点被点击
             treeNodeClick (type, data, node, store) {
                 if (type == 1) {
+                    if (this.nodeSelected && this.nodeSelected.value === data.value) return
                     this.nodeParentSelected = node.parent// 记录父节点
                     this.nodeSelected = data // 记录当前节点
                     this.$refs.uploadImg.clearFiles()
