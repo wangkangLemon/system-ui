8e31e0f45bb7bb83e353cbdda87267769c80b8dc
huanghuixin <274919800@qq.com>
2017-05-08 11:53:46 +0800

    优化日期范围组件的代码

--- assets/src/views/component/select/CourseAlbum.vue
+++ assets/src/views/component/select/CourseAlbum.vue
@@ -1,6 +1,6 @@
 <!--课程专辑下拉框-->
 <template>
-    <SelectScroll :changeCb="handleChange" :requestCb="fetchData" :placeholder="placeholder" v-model="currVal">
+    <SelectScroll :changeCb="handleChange" :requestCb="fetchData" :placeholder="placeholder" v-model="currVal" :disabled="disabled">
     </SelectScroll>
 </template>
 
@@ -8,7 +8,16 @@
     import courseService from '../../../services/courseService'
     import SelectScroll from '../../component/form/SelectScroll.vue'
     export default{
-        props: ['value', 'change', 'placeholder'],
+        props: {
+            value: [String, Number],
+            change: Function,
+            placeholder: String,
+            list: Array,
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+        },
         components: {
             SelectScroll
         },
