d567551a962988f52fdf20e7267c9f4336a59bac
huangrui <huangrui@vodjk.com>
2017-05-25 13:37:04 +0800

    文章分类组件调整

--- assets/src/views/component/select/ArticleCategory.vue
+++ assets/src/views/component/select/ArticleCategory.vue
@@ -1,17 +1,19 @@
-<style lang="scss" type="stylesheet/scss">
-    .el-cascader-menu {
-        height: auto !important;
+<!--课程栏目-->
+<style lang="scss">
+    .course-select-container {
+        .el-icon-caret-bottom {
+            text-align: center;
+            text-align-last: center;
+        }
     }
 </style>
-<!--文章分类-->
 <template>
-    <el-cascader
-            :options='options' :show-all-levels="false"
-            @active-item-change="handleItemChange"
-            :clearable="true"
-            :props="props"
-            @change="setCurrVal"
-            :placeholder="placeholder"
+    <el-cascader class="course-select-container" ref="container" v-loading="loading"
+                 :options='options' :show-all-levels="false"
+                 :placeholder="placeholder"
+                 @active-item-change="handleItemChange"
+                 :clearable="true"
+                 @change="setCurrVal"
     ></el-cascader>
 </template>
 
@@ -20,54 +22,97 @@
     import ArticleService from '../../../services/articleService'
     export default{
         props: {
-            value: [String, Number],
+            value: [String, Number, Array],
+            autoClear: {
+                type: Boolean,
+                default: false
+            },
             onchange: Function,
             placeholder: {
                 type: String,
                 default: '请选择'
             },
+            showNotCat: { // 是否显示未分类的选项
+                type: Boolean,
+                default: true
+            }
         },
         data () {
             return {
+                loading: false,
                 options: [],
                 currVal: this.value,
-                props: {
-                    value: 'id',
-                    children: 'children',
-                    label: 'name'
-                }
+                companyid: void 0, // 企业id
+                lastData: void 0,
             }
         },
-        created () {
-            ArticleService.getCategoryTree({}).then((ret) => {
-                ret.map((item) => {
-                    item.children = item.has_children ? [{label: '加载中...'}] : null
-                })
-                this.options = ret
+        watch: {
+            'value' (val) {
+                this.setCurrVal(val)
+            },
+            'currVal' (val, old) {
+                this.$emit('input', val.length > 0 ? parseInt(val[val.length - 1]) : val)
+                this.onchange && this.onchange(val)
+            }
+        },
+        mounted () {
+            this.$refs.container.$el.addEventListener('click', () => {
+                if (this.loading || this.options.length > 0) return
+                if (this.lastData) {
+                    this.options = this.lastData
+                } else {
+                    this.loading = true
+                    ArticleService.getCategoryTree({companyid: this.companyid})
+                        .then(ret => {
+                            // 不显示未分类那一项
+                            if (!this.showNotCat) {
+                                ret = ret.filter((item) => {
+                                    return item.id != 0
+                                })
+                            }
+
+                            this.options = treeUtils.arr2Cascader(ret, 0, void 0, void 0, 'name', 'id')
+                            this.loading = false
+                        })
+                }
             })
+        },
+        deactivated () {
+            if (this.autoClear && this.options.length > 0) {
+                this.lastData = this.options
+                this.options = []
+            }
+        },
+        activated () {
         },
         methods: {
             setCurrVal (val) {
+                if (!val) this.$refs.container.clearValue(new window.Event('click'))
                 if (this.currVal == val || !val) return
                 this.currVal = val
-                this.$emit('input', val[val.length - 1])
-                this.onchange && this.onchange(val)
             },
             handleItemChange (val) {
                 if (val.length < 1) return
                 // 递归找到该项
                 let currItem = treeUtils.findItem(this.options, val, 'value')
+
                 if (!currItem.children || (currItem.children.length > 0 && currItem.children[0].value)) return
-                ArticleService.getCategoryTree({
-                    id: val[val.length - 1]
-                }).then((ret) => {
-                    // 重新组合数据
-                    ret.map((item) => {
-                        item.children = item.has_children ? [{label: '加载中...'}] : null
+                ArticleService.getCategoryTree({companyid: this.companyid, id: val[val.length - 1]})
+                    .then(ret => {
+                        // 重新组合数据
+                        ret.map((item) => {
+                            item.label = item.name
+                            item.value = item.id
+                            item.children = item.has_children ? [{label: '加载中...'}] : null
+                        })
+                        currItem.children = ret
                     })
-                    currItem.children = ret
-                })
             },
-        }
+            clearData () {
+                this.lastData = this.options
+                this.options = []
+            }
+        },
+        components: {}
     }
 </script>
