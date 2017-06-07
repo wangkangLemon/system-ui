f64a109c3674142aa753c848bc2add4c86139dc8
huanghuixin <274919800@qq.com>
2017-06-07 18:47:20 +0800

    build环境 不进行url填充

--- assets/src/utils/filterUtils.js
+++ assets/src/utils/filterUtils.js
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import * as timeUtils from '../utils/timeUtils'
 import config from '../../config/config'
+import appConfig from './config'
 
 // 默认头像
 export const defaultAvatar = Vue.filter('defaultAvatar', function (urlObj) {
@@ -28,7 +29,7 @@ export const tamp2Str = Vue.filter('tamp2Str', function (timetamp) {
 
 // 填充图片的url
 export const fillImgPath = Vue.filter('fillImgPath', function (url) {
-    if (!url) return url
+    if (!url && !appConfig.debug) return url
     let apiUrl = config.API_HOST_PROD.replace(/(^")|("$)/g, '')
     return url.indexOf('http://') > -1 ? url : apiUrl + url
 })
