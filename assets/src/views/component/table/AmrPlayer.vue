961e3a597fafd8731c30895aeed2d2db2ea7444c
gaohuijun <gaohj@vodjk.com>
2017-06-29 09:06:25 +0800

    修复播音失败

--- assets/src/views/component/table/AmrPlayer.vue
+++ assets/src/views/component/table/AmrPlayer.vue
@@ -22,9 +22,6 @@ export default {
     },
     data() {
         return {
-            instance: null,
-            // scriptTagStatus -> 0:代码未加载，1:两个代码依赖加载了一个，2:两个代码依赖都已经加载完成
-            scriptTagStatus: 0,
             currVal: '',
             downFlag: 0,
             downLoading: false,
@@ -32,20 +29,8 @@ export default {
     },
     created() {
         this.currVal = this.url
-        if (window.AmrPlayer !== undefined) {
-            // 如果全局对象存在，说明编辑器代码已经初始化完成，直接加载编辑器
-            this.scriptTagStatus = 2
-            this.initPlayer()
-        } else {
-            // 如果全局对象不存在，说明编辑器代码还没有加载完成，需要加载编辑器代码
-            this.insertScriptTag()
-        }
-    },
-    beforeDestroy() {
-        // 组件销毁的时候，要销毁 Player 实例
-        if (this.instance !== null && this.instance.destroy) {
-            this.instance.destroy()
-        }
+        // 如果全局对象不存在，说明编辑器代码还没有加载完成，需要加载编辑器代码
+        this.insertScriptTag()
     },
     methods: {
         insertScriptTag() {
@@ -65,60 +50,9 @@ export default {
                 s.appendChild(armScriptTag)
                 s.appendChild(playerScriptTag)
             }
-            // 等待代码加载完成后初始化编辑器
-            if (playerScriptTag.loaded) {
-                this.scriptTagStatus++
-            } else {
-                playerScriptTag.addEventListener('load', () => {
-                    this.scriptTagStatus++
-                    playerScriptTag.loaded = true
-                    this.initPlayer()
-                })
-                playerScriptTag.onerror = () => {
-                    xmview.showTip('error', '加载AMR播放器失败, 请刷新重试!')
-                }
-            }
-            if (armScriptTag.loaded) {
-                this.scriptTagStatus++
-            } else {
-                armScriptTag.addEventListener('load', () => {
-                    this.scriptTagStatus++
-                    armScriptTag.loaded = true
-                    this.initPlayer()
-                })
-                armScriptTag.onerror = () => {
-                    xmview.showTip('error', '加载AMR播放器失败, 请刷新重试!')
-                }
-            }
-            this.initPlayer()
-        },
-        initPlayer() {
-            // scriptTagStatus 为 2 的时候，说明两个必需引入的 js 文件都已经被引入，且加载完成
-            if (this.scriptTagStatus === 2 && this.instance === null) {
-                // Vue 异步执行 DOM 更新，这样一来代码执行到这里的时候可能 template 里面的 script 标签还没真正创建
-                // 所以，我们只能在 nextTick 里面初始化 ArmPlayer
-                this.$nextTick(() => {
-                    this.instance = new window.AmrPlayer()
-                })
-            }
         },
         play() {
-            if (this.instance !== null) {
-                if (this.downFlag === 0) {
-                    this.downLoading = true
-                    this.instance.playAmr(this.currVal)
-                    this.downFlag++
-                    setTimeout(this.close, 1500)
-                } else {
-                    this.instance.toggle()
-                }
-            } else {
-                xmview.showTip('error', '加载AMR播放器失败, 请刷新重试!')
-            }
-        },
-        close() {
-            this.downLoading = false
-            this.instance.toggle()
+            window.playAmr(this.currVal)
         },
     }
 }
--- public/vendor/amr/amrplayer.js
+++ public/vendor/amr/amrplayer.js
@@ -2,134 +2,78 @@
 * -- AmrPlayer --
 * params:
 *  1. amr_url
-*  2. download_success_cb (optional)
-*  3. download_progress_cb (optional)
-* props:
-*  1. bool canPlay
-*  2. bool isPlaying
-* methods:
-*  1. play()
-*  2. pause()
-*  3. toggle() // play() when paused or pause() when playing
-*  3. endWith(callback) // fire callback with ended event
-* */
-window.AmrPlayer = function(download_success_cb, download_progress_cb){
-    this.init(download_success_cb, download_progress_cb);
-};
-AmrPlayer.prototype = {
-    init: function(){
-        this.audioContext = null;
-        this.bufferSource = null;
-        this.blob = null;
-        this.canPlay = false;
-        this.isPlaying = false;
-        var cnt = 0;
-        this.ended_cb = function(){
-            if(cnt === 0){
-                cnt++;
-            }
-        };
-        
-    },
-    playAmr: function(amr_url,download_success_cb, download_progress_cb) {
-        this.downloadAmrBlob(amr_url, download_success_cb, download_progress_cb);
-    },
-    downloadAmrBlob: function(amr_url, download_success_cb, download_progress_cb){
-        var self = this;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', amr_url);
-        xhr.responseType = 'blob';
-        xhr.onreadystatechange = function(e) {
-            if ( xhr.readyState == 4 && xhr.status == 200 ) {
-                self.blob = new Blob([xhr.response], {type: 'audio/mpeg'});
-                self.genPLayer();
-                self.canPlay = true;
-                download_success_cb && download_success_cb();
-            }
-        };
-        xhr.onprogress = function(e){
-            if(e.lengthComputable){
-                download_progress_cb && download_progress_cb(e);
-            }
-        };
-        xhr.send();
-    },
-    genPLayer: function(){
-        var self = this;
-        this.isPlaying = false;
-        this.readBlob(this.blob, function(data){
-            self.readAmrArray(data);
-        });
-    },
-    readBlob: function(blob, callback) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var data = new Uint8Array(e.target.result);
-            callback(data);
-        };
-        reader.readAsArrayBuffer(blob);
-    },
-    readAmrArray: function(array) {
-        var samples = AMR.decode(array);
-        if (!samples) {
-            alert('Failed to decode!');
-            return;
-        }
-        this.readPcm(samples);
-    },
-    readPcm: function(samples) {
-        var self = this;
-        var ctx = this.getAudioContext();
-        this.bufferSource = ctx.createBufferSource();
-        var buffer = ctx.createBuffer(1, samples.length, 8000);
-        if (buffer.copyToChannel) {
-            buffer.copyToChannel(samples, 0, 0)
-        } else {
-            var channelBuffer = buffer.getChannelData(0);
-            channelBuffer.set(samples);
-        }
-        this.bufferSource.buffer = buffer;
-        this.bufferSource.connect(ctx.destination);
-        this.bufferSource.onended = function(){
-            self.ended_cb && self.ended_cb();
-            self.genPLayer();
-        };
-    },
-    getAudioContext: function(){
-        if (!this.audioContext) {
-            if(window.AudioContext) {
-                this.audioContext = new AudioContext();
-            } else {
-                this.audioContext = new window.webkitAudioContext();
-            }
-        }
-        return this.audioContext;
-    },
-    play: function(){
-        if( !this.isPlaying && this.canPlay ){
-            this.bufferSource.start();
-            this.isPlaying = true;
-        }
-        else{
-            this.warn('can not play now');
-        }
-    },
-    pause: function(){
-        if( this.isPlaying && this.canPlay ) {
-            this.bufferSource.stop();
-            this.genPLayer();
-        }
-        else{
-            this.warn('can not pause now');
-        }
-    },
-    toggle: function(){
-        this.isPlaying ? this.pause() : this.play();
-    },
-    endedWith: function(cb){
-        this.ended_cb = cb;
-    },
-    warn: function(msg){
-        console.warn(msg);
+*/
+function playAmr(url) {
+    fetchBlob(url, function (blob) {
+        playAmrBlob(blob);
+    });
+}
+var gAudioContext = new AudioContext();
+
+function getAudioContext() {
+    if (!gAudioContext) {
+        gAudioContext = new AudioContext();
     }
-};
+    return gAudioContext;
+}
+
+function fetchBlob(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.response);
+        }
+    };
+    xhr.onerror = function () {
+        alert('Failed to fetch ' + url);
+    };
+    xhr.send();
+}
+
+function readBlob(blob, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var data = new Uint8Array(e.target.result);
+        callback(data);
+    };
+    reader.readAsArrayBuffer(blob);
+}
+
+function fetchAndReadBlob(url, callback) {
+    fetchBlob(url, function (blob) {
+        readBlob(blob, callback);
+    });
+}
+
+function playAmrBlob(blob, callback) {
+    readBlob(blob, function (data) {
+        playAmrArray(data);
+    });
+}
+
+function playAmrArray(array) {
+    var samples = AMR.decode(array);
+    if (!samples) {
+        alert('Failed to decode!');
+        return;
+    }
+    playPcm(samples);
+}
+
+function playPcm(samples) {
+    var ctx = getAudioContext();
+    var src = ctx.createBufferSource();
+    var buffer = ctx.createBuffer(1, samples.length, 8000);
+    if (buffer.copyToChannel) {
+        buffer.copyToChannel(samples, 0, 0)
+    } else {
+        var channelBuffer = buffer.getChannelData(0);
+        channelBuffer.set(samples);
+    }
+
+    src.buffer = buffer;
+    src.connect(ctx.destination);
+    src.start();
+}
