93a05fdc1fff46acb35a80f6464a5779a312f996
huangrui <huangrui@vodjk.com>
2017-08-04 18:06:11 +0800

    bug修复

--- assets/src/services/api.js
+++ assets/src/services/api.js
@@ -84,10 +84,6 @@ function sendRequest (method, url, params, needLoding = false) {
     if (method === 'GET')
         url = url + '?' + processParams(params)
 
-    if (method === 'PUT' || method === 'POST') {
-        params = params.replace(/%/g, '%25')
-    }
-
     needLoding && xmview.setLoading(true)
 
     let pRequest = new Promise((resolve, reject) => {
