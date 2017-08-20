05810761cb15562b5e8ccfe0e9eae04ee248e2af
coldstar <safeie@163.com>
2017-08-20 14:04:10 +0800

    完善设置签约统计

--- assets/src/views/system/tools/SignAnalytics.vue
+++ assets/src/views/system/tools/SignAnalytics.vue
@@ -11,21 +11,64 @@
         text-align: right;
         margin-top: 10px;
     }
+    .form-footer {
+        text-align: center;
+    }
 }
 </style>
 <template>
     <main id="system-tools-sign-analytics-container">
-        <div>签约信息</div>
-        <el-form :inline="true" :model="data" class="demo-form-inline">
-            <el-form-item label="门店总数">
-                <el-input v-model="data.department_total" placeholder="门店总数"></el-input>
-            </el-form-item>
-            <el-form-item label="今日新增">
-                <el-input v-model="data.department_today" placeholder="今日新增"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">保存</el-button>
-            </el-form-item>
+        <el-form :model="data" :rules="rules" ref="form">
+            <el-row>
+                <el-col :span="2">
+                    <el-form-item prop="" label="门店统计">
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="department_total" label="门店总数" :label-width="labelWidth">
+                        <el-input v-model="data.department_total" placeholder="门店总数"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="department_today" label="今日新增" :label-width="labelWidth">
+                        <el-input v-model="data.department_today" placeholder="今日新增"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="department_yesterday" label="昨日新增" :label-width="labelWidth">
+                        <el-input v-model="data.department_yesterday" placeholder="昨日新增"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="1"></el-col>
+            </el-row>
+
+            <el-row>
+                <el-col :span="2">
+                    <el-form-item prop="" label="店员统计">
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="user_total" label="店员总数" :label-width="labelWidth">
+                        <el-input v-model="data.user_total" placeholder="店员总数"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="user_today" label="今日新增" :label-width="labelWidth">
+                        <el-input v-model="data.user_today" placeholder="今日新增"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="7">
+                    <el-form-item prop="user_yesterday" label="昨日新增" :label-width="labelWidth">
+                        <el-input v-model="data.user_yesterday" placeholder="昨日新增"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="1"></el-col>
+            </el-row>
+
+            <div class="form-footer">
+                <el-button type="primary" @click="submit('form')">确 定</el-button>
+            </div>
+
         </el-form>
     </main>
 </template>
@@ -37,14 +80,23 @@ export default {
     components: {},
     data() {
         return {
+            labelWidth: '100px',
             data: {
                 department_total: 0,
                 department_today: 0,
                 department_yesterday: 0,
-                user_today: 0,
                 user_total: 0,
+                user_today: 0,
                 user_yesterday: 0,
-            }
+            },
+            rules: {
+                department_total: [
+                    { type: 'number', required: true, message: '必须填写', trigger: 'change' }
+                ],
+                user_total: [
+                    { type: 'number', required: true, message: '必须填写', trigger: 'change' }
+                ],
+            },
         }
     },
     created() {
@@ -60,11 +112,21 @@ export default {
     methods: {
         getData() {
             return ToolsService.getSignAnalytics().then((ret) => {
-                this.data.department_total = ret.data.department_total
+                this.data = ret.data
             })
         },
-        onSubmit() {
-
+        submit(form) {
+            this.$refs[form].validate((valid) => {
+                if (valid) {
+                    ToolsService.setSignAnalytics(this.data).then((ret) => {
+                        xmview.showTip('success', '操作成功')
+                    }).catch((ret) => {
+                        xmview.showTip('error', ret.message)
+                    })
+                } else {
+                    return false
+                }
+            })
         }
     }
 
