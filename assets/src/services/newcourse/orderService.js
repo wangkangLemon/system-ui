ee099e5f07d8b4f2968ac6b60900c3361497d78a
coldstar <safeie@163.com>
2017-08-29 16:43:25 +0800

    修复课程订单接口默认参数

--- assets/src/services/newcourse/orderService.js
+++ assets/src/services/newcourse/orderService.js
@@ -4,9 +4,15 @@ const urlPre = config.apiHost + '/newcourse/order'
 
 class OrderService {
     // 搜索
-    search({ course_id, user_id, user_mobile, pay_method, status, in_compnay, time_start, time_end, page, page_size }) {
+    search({ course_id, user_id, user_mobile, pay_method, status, in_company, time_start, time_end, page, page_size }) {
+        if (in_company == '' || in_company == undefined) {
+            in_company = -1
+        }
+        if (status == '' || status == undefined) {
+            status = -1
+        }
         let url = urlPre + '/search'
-        return api.get(url, { course_id, user_id, user_mobile, pay_method, status, in_compnay, time_start, time_end, page, page_size }, false).then(ret => {
+        return api.get(url, { course_id, user_id, user_mobile, pay_method, status, in_company, time_start, time_end, page, page_size }, false).then(ret => {
             console.log(ret)
             if (ret.code == 0) {
                 return ret.data
