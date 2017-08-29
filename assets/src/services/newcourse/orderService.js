5416cc2d8b21211eff89a3ec5dc1dd65d296de7d
coldstar <safeie@163.com>
2017-08-29 14:42:06 +0800

    修改课程订单，增加 有无归属参数

--- assets/src/services/newcourse/orderService.js
+++ assets/src/services/newcourse/orderService.js
@@ -4,9 +4,9 @@ const urlPre = config.apiHost + '/newcourse/order'
 
 class OrderService {
     // 搜索
-    search({ course_id, user_id, user_mobile, pay_method, status, time_start, time_end, page, page_size }) {
+    search({ course_id, user_id, user_mobile, pay_method, status, in_company, time_start, time_end, page, page_size }) {
         let url = urlPre + '/search'
-        return api.get(url, { course_id, user_id, user_mobile, pay_method, status, time_start, time_end, page, page_size }, false).then(ret => {
+        return api.get(url, { course_id, user_id, user_mobile, pay_method, status, in_company, time_start, time_end, page, page_size }, false).then(ret => {
             if (ret.code == 0) {
                 return ret.data
             } else {
