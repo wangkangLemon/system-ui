bc9958434dd81a3521f1e0b27713aa2511046763
kangcc <kangcc@vodjk.com>
2018-05-21 14:20:00 +0800

    【执业药师-优惠设置】... https://www.tapd.cn/20605431/s/1001198 --bug=1002412 --user=kangchen

--- assets/src/views/yshi/component/GoodsList.vue
+++ assets/src/views/yshi/component/GoodsList.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <el-table class="data-table" :data="data" :fit="true" border style="margin-top: 5px;">
+        <el-table class="data-table" :data="list" :fit="true" border style="margin-top: 5px;">
             <el-table-column 
                 label="类型" 
                 :prop="columnType"
@@ -14,6 +14,16 @@
                 :prop="columnName">
             </el-table-column>
         </el-table>
+        <el-pagination 
+            class="block"
+            @size-change="val=> {fetchParam.page_size=val; fetchData()}"
+            @current-change="val=> {fetchParam.page=val; fetchData()}"
+            :current-page="fetchParam.page"
+            :page-size="fetchParam.page_size"
+            :page-sizes="[10, 15, 30, 60, 100]"
+            layout="sizes, total, prev, pager, next" 
+            :total="total">
+        </el-pagination>
         <el-tag>总计：{{data.length}}个商品</el-tag>
     </main>
 </template>
@@ -29,6 +39,30 @@
             columnName: {
                 type: String,
                 default: 'goods_name'
+            }
+        },
+        created () {
+            this.fetchData()
+        },
+        watch: {
+            data () {
+                this.total = this.data.length
+                this.fetchData()
+            }
+        },
+        data () {
+            return {
+                fetchParam: {
+                    page: 1,
+                    page_size: 10,
+                },
+                list: this.data,
+                total: this.data.length,
+            }
+        },
+        methods: {
+            fetchData () {
+                this.list = this.data.slice((this.fetchParam.page - 1) * this.fetchParam.page_size, this.fetchParam.page * this.fetchParam.page_size)
             }
         }
     }
