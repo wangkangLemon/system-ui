772dbbf376fc5b55203a59585b4d0f5fd413c316
xudong <xudong@vodjk.com>
2018-07-12 16:44:43 +0800

    财务管理 课程订单页面修改

--- assets/src/services/newcourse/orderService.js
+++ assets/src/services/newcourse/orderService.js
@@ -4,9 +4,10 @@ const urlPre = config.apiHost + '/newcourse/order'
 
 class OrderService {
     // 搜索
-    search({ course_id, user_id, user_mobile, pay_method, status, object_type, in_company, time_start, time_end, page, page_size }) {
+    search({ user_id, trade_no, pay_method, status, order_type, object_type, in_company, start_date, end_date, page, page_size }) {
+        // let url = config.apiHost + '/order/search'
         let url = urlPre + '/search'
-        return api.get(url, { course_id, user_id, user_mobile, pay_method, status, object_type, in_company, time_start, time_end, page, page_size }, false).then(ret => {
+        return api.get(url, { user_id, trade_no, pay_method, status, order_type, object_type, in_company, start_date, end_date, page, page_size }, false).then(ret => {
             if (ret.code == 0) {
                 return ret.data
             } else {
@@ -20,6 +21,18 @@ class OrderService {
         let url = `${urlPre}/${id}/note`
         return api.put(url, {note}).then(ret => {
             if (ret.code) {
+                return Promise.reject(ret)
+            }
+        })
+    }
+
+    // 获取用户列表
+    userList (keyword, page, page_size) {
+        let url = config.apiHost + '/common/user/search'
+        return api.get(url, {keyword, page, page_size}, false).then(ret => {
+            if (ret.code == 0) {
+                return ret.data
+            } else {
                 return Promise.reject(ret)
             }
         })
